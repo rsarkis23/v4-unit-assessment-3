69468638afbfd8bbd5cad4900daabcb82501bd0a
rsarkis23 <learning.r2@gmail.com>
2021-03-05 19:11:51 -0500

    React Step-3

--- bookist/src/App.js
+++ bookist/src/App.js
@@ -1,17 +1,31 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header'
 import BookList from './Components/BookList'
 import Shelf from './Components/Shelf'
+import data from './data'
 
-function App() {
+
+
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      books: data
+    }
+  }
+
+  render(){
   return (
-    <div className="App">
-      <Header />
-      <BookList />
-      <Shelf />
-    </div>
-  );
+      <div className="App">
+        <Header />
+        <BookList />
+        <Shelf />
+      </div>
+    )
+  }
 }
 
 export default App;
